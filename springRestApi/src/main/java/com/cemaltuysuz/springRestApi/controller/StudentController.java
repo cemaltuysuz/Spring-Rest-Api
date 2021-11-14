d651a1bbd3dab10f10ad37fc0621be964e76e93b
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-11-14 22:56:06 +0300

    ✨ inserted get mapping for get student by id and put mapping for update student 🚀

--- springRestApi/src/main/java/com/cemaltuysuz/springRestApi/controller/StudentController.java
+++ springRestApi/src/main/java/com/cemaltuysuz/springRestApi/controller/StudentController.java
@@ -1,8 +1,10 @@
 package com.cemaltuysuz.springRestApi.controller;
 
 import com.cemaltuysuz.springRestApi.entity.Student;
+import com.cemaltuysuz.springRestApi.exception.ResourceNotFoundException;
 import com.cemaltuysuz.springRestApi.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,5 +24,23 @@ public class StudentController {
     @PostMapping
     public Student createStudent(@RequestBody Student student){
         return studentRepository.save(student);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Student> getStudentById (@PathVariable long id){
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not exist with id:"+id));
+        return ResponseEntity.ok(student);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Student> updateStudent (@PathVariable long id,@RequestBody Student studentDetails){
+        Student updateStudent = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not exist with id:"+id));
+        updateStudent.setName(studentDetails.getName());
+        updateStudent.setLastName(studentDetails.getLastName());
+        updateStudent.setAge(studentDetails.getAge());
+        studentRepository.save(updateStudent);
+        return ResponseEntity.ok(updateStudent);
     }
 }
